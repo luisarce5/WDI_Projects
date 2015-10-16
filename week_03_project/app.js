6cdbec226c1047bf94bd1777a5d30ae43fe0b475
LUIS ARCE <luisarce5@gmail.com>
2015-10-15 21:11:51 -0400

    added match function; partial checkWin function

--- week_03_project/app.js
+++ week_03_project/app.js
@@ -1,75 +1,64 @@
-
 window.onload = function () {
   console.log ("HTML file linked to JavaScript file");
+  $('#button1').on('click', makeCard);
+  $('#button2').on('click', {answer: "yes"}, checkWin);
+  $('#button3').on('click', {answer: "partial"}, checkWin);
+  $('#button4').on('click', {answer: "no"}, checkWin);
+  };
 
-  // $("#button1").click(function() {
-  //     makeCard();
-  //   });
+var checkWin = function (event) {
+  console.log ("Clicked on " + event.data.answer);
+  // var response = event.data.answer;
+  // if (response == match () ) {
+  //   console.log ("Player wins")
+  // } else {
+  //   console.log ("Player loses")
   // };
-$("#button1").on("click", makeCard);
 };
 
 var startGame = function () {
 };
 
+var color_index = ['red','blue','green','yellow'];
+var shape_index = ['circle','square','triangle','diamond'];
 var cardLog = [];
 //  will store the properties (color & shape) of the cards displayed since Game Start
-// element index 0 = previous card array
-// element index 1 = current card array
-// card array => 1st element = color ; 2nd element = shape
-
-var randomColor = false;
+// cardLog array => each index number contains color and shape
 
 var makeCard = function () {
   console.log ("previous cardLog is " + cardLog);
   var container = $('.container2'); // select the div that will hold the Card
   var shape = giveShape(); // assignment of random shape
-  console.log ("the shape is " + shape);
   // if (randomColor == true) {
     // in the Game's first level only the shapes are randomly generated,
     // so each color is fixed to a specific shape.  That is, the cards will
     // only show red circle, blue square, green XXXcircle or yellow XXXsquare
-    var color = giveColor(); // assignment of random color
-  var newCardData = [color[0],shape[0]]; // store color & shape properties in array
+  var color = giveColor(); // assignment of random color
+  var newCardData = [color,shape]; // store color & shape properties in array
   // add class of color number to newCard div  // pending as perhaps not required
   // add class of shape number to newCard div // pending as perhaps not required
   $( '.container2').empty(); // clean all the content of the div that will hold the Card
   //document.getElementById(elementID).innerHTML = "";
-  var newCard = $('<div>').addClass(shape[1]); // create a new div and add the class of 'shape'
-  newCard.css('background-color', color[1]); //set the background color of that div
+  var newCard = $('<div>').addClass(shape_index[shape]); // create a new div and add the class of 'shape'
+  newCard.css('background-color', color_index[color]); //set the background color of that div
   newCard.appendTo(container); // Append the Card to the Container2
   console.log ("newCardData is " + newCardData);
   cardLog.push(newCardData);
   console.log ("new cardLog is " + cardLog);
+  match ();
 };
 
 var giveColor = function () { // generates a random color
-  //returns an array: 1st element = color number; 2nd element = color as text
-    color_number = Math.ceil(Math.random()*4);
-    // console.log (color_number);
-    switch (color_number) {
-      case 1: return [color_number,"red"];
-      case 2: return [color_number,"blue"];
-      case 3: return [color_number,"green"];
-      case 4: return [color_number,"yellow"];
-    }; // close switch
-}; // close function
-
-var giveShape = function () { // generates a random shape
-  //returns an array: 1st element = shape number; 2nd element = shape as text
-  shape_number = Math.ceil(Math.random()*4);
-  switch (shape_number) {
-    case 1: return [shape_number,"circle"];
-    case 2: return [shape_number,"square"];
-    case 3: return [shape_number,"triangle"];
-    case 4: return [shape_number,"diamond"];
-  };
+  color_number = Math.floor(Math.random() * color_index.length);
+  return color_number;
 };
 
-var shapes = ['circle','square','triangle','diamond'];
-var colors = ['red','blue','green','yellow'];
+var giveShape = function () { // generates a random shape
+  shape_number = Math.floor(Math.random() * shape_index.length);
+  return shape_number;
+};
 
-var Move = function () {
+var move = function () {
   makeCard ();
   // Player indicates if the card matches preceding card
   // Check for Match
@@ -77,38 +66,30 @@ var Move = function () {
   // Update Board indicating Match result
 };
 
-var Match = function () {
-  cardLog.length
-
+var match = function () {
 // checks if the newly generated random Card
 // matches the previously displayed random Card
-};
+// 'Complete' Match: shape and color both match
+// 'Partial' Match: either shapes match or colors match
+  var lastCard = cardLog[cardLog.length-1];
+  var prevCard = cardLog[cardLog.length-2];
+  console.log (prevCard);
+  console.log (lastCard);
+  if (lastCard[0] == prevCard[0] & lastCard[1] == prevCard[1]) {
+    console.log ("Complete Match");
+    return "yes";
+  } else if (lastCard[0] == prevCard[0] || lastCard[1] == prevCard[1]) {
+    console.log ("Partial Match");
+    return "partial";
+  } else {
+    console.log ("No Match");
+    return "no";
+  };
+}; // the first time Match is run at the Game start,
 
-var checkWin = function () {
-  // checks if player's input coincides with Match output
-};
 
 var score = function () {
 };
 
 var timer = function () {
-}
-
-// TEMP TESTS
-// console.log ("newCardData is " + newCardData);
-// var temp = cardLog[1];
-// console.log ("temp value is " + cardLog[1]);
-// cardLog.splice(0,1,cardLog[1]);
-// console.log ("interm cardLog is " + cardLog);
-// // array.splice(startIndex, howManyElements, newItem)
-// // cardLog[0] = cardLog[1];
-// cardLog.splice(1,1,newCardData);
-// console.log("new cardLog is " + cardLog);
-// cardLog = [[1,2],
-//            [3,4]
-//          ];
-// console.log (cardLog);
-// newCard = [5,6];
-// console.log (newCard);
-// cardLog.splice (1, 1, newCard);
-// console.log (cardLog);
+};
